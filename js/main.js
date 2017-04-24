9a91e16defc9d7ef10fb0ea45bf0d54e7969f8c5
kavitasri5 <kavitasri5@gmail.com>
2017-04-24 18:04:09 +0530

    this is my first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="main.js" />
+var app = angular.module('My.Controllers',[]);
+
+app.controller("myController", function ($scope) {
+    $scope.data = "my first Dependency controller";
+})
+
+app.controller("myCtrl", function ($scope) {
+    $scope.message = "welcome to Home Page";
+});
+
+app.controller("aboutCtrl", function ($scope) {
+    $scope.message = "welcome to About Us";
+});
+
+app.controller("blogCtlr", function ($scope) {
+    $scope.message = "welcome to Blog";
+});
+
+app.controller("contactCtrl", function ($scope) {
+    $scope.message = "welcome to Contact Us";
+});
---
+++ js/route.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="angular.min.js" />
+
+var app = angular.module("Myapp", ["ngRoute","My.Controllers"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/home", {
+        template: "<h1>Home Page</h1><p>Click on the links to change this content</p>"
+    })
+    .when("/about", {
+        template: "<h1>About Us</h1><p>About us page content</p>"
+    })
+    .when("/blog", {
+        template: "<h1>blog Page</h1><p>Blog content</p>"
+    })
+    .when("/contact", {
+        template: "<h1>Contact Us</h1><p>Contact Us Page Content</p>"
+    });
+});
